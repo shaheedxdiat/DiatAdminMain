651c2594f8302d1eac8c4f2943e7abbc8f8d987e
RAHEEMUDHEEN-MA <raheemudheenma118@gamil.com>
2024-03-08 05:33:00 +0530

    test2

--- src/pages/SelectCourse.jsx
+++ src/pages/SelectCourse.jsx
@@ -1,8 +1,12 @@
+import { Button } from 'react-bootstrap'
 import React from 'react'
+import { supabase } from '../SupaBase'
 
 const SelectCourse = () => {
+
   return (
     <div>
+      <Button onClick={async()=>{supabase.auth.signOut()}}> logout</Button>
       <input type="text" placeholder='course selection' />
     </div>
   )
