6fe54a9d45fe4b46c23ae16a1f094ff81ef6f30b
raheemudheen M A <raheemudheenma118@gmail.com>
2024-03-10 03:17:17 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,7 @@ const NavBar = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   
-  // const [session, setSession] = useState(null)
+  
 
     useEffect(() => {
       supabase.auth.getSession().then(({ data: { session } }) => {
