6f7fddf3daaedf094ebb8f8bcb45782d5f897125
RAHEEMUDHEEN-MA <raheemudheenma118@gamil.com>
2024-03-10 01:35:58 +0530

    day 2 hosting -night-photo upload2

--- src/pages/StudentRegistration.jsx
+++ src/pages/StudentRegistration.jsx
@@ -4,6 +4,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { supabase } from "../SupaBase";
 import { genarateStudentId } from "../genarateID";
 import states from "../states&dists/data.json";
+import { isDisabled } from "@testing-library/user-event/dist/utils";
 
 const StudentRegistration = () => {
   const navigate = useNavigate();
@@ -26,10 +27,11 @@ const StudentRegistration = () => {
   const [hostlite, sethostlite] = useState(false);
   const [photoURL, setphotoURL] = useState("");
   const [GenaratedID, setGenaratedID] = useState("");
-  const [File, setFile] = useState()
+  const [File, setFile] = useState();
 
-  const [photoUploadWarning, setphotoUploadWarning] = useState(false)
-  console.log(GenaratedID);
+  const [photoUploadWarning, setphotoUploadWarning] = useState(false);
+  const [uploaded, setuploaded] = useState(false);
+  // console.log(GenaratedID);
 
   const distObj = states.states.find((data) => data.state === state);
 
@@ -65,11 +67,15 @@ const StudentRegistration = () => {
     navigate(-1);
   };
   const handleUpload = async (e) => {
-    if (name==="") {
-      setphotoUploadWarning(true)
-      return
+    if (name === "") {
+      setphotoUploadWarning(true);
+      return;
+    } else if (File.size / 1024 > 150) {
+      alert("too large");
+      return;
     }
-    
+
+    console.log(parseInt(File.size / 1024), "KB");
 
     const { data, error } = await supabase.storage
       .from("student_photos")
@@ -78,14 +84,14 @@ const StudentRegistration = () => {
           console.error(error)
           return
       }
+      setFile(null)
+    setuploaded(true);
       setphotoURL(`https://qlterlkavzxidliounaa.supabase.co/storage/v1/object/public/${data.fullPath}`)
       console.log("uploaded",data)
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
-   
 
     const { data, error } = await supabase
       .from("students")
@@ -147,7 +153,7 @@ const StudentRegistration = () => {
           Back
         </Button>
       </div>
-      <h4 style={{ color: "orangered" }}>NEW ADMISSION</h4>
+      <h4 style={{ color: "orange" }}>NEW ADMISSION</h4>
 
       <div style={{ padding: "30px" }}>
         <Form onSubmit={handleSubmit}>
@@ -159,7 +165,10 @@ const StudentRegistration = () => {
                 type="text"
                 placeholder="Name"
                 value={name}
-                onChange={(e) => {setName(e.target.value.toUpperCase());setphotoUploadWarning(false)}}
+                onChange={(e) => {
+                  setName(e.target.value.toUpperCase());
+                  setphotoUploadWarning(false);
+                }}
               />
             </Form.Group>
             <Form.Group as={Col} md="4" controlId="validationCustom02">
@@ -276,13 +285,51 @@ const StudentRegistration = () => {
                 required
                 type="file"
                 placeholder="Photo"
-           
-                onChange={(e)=>{setFile(e.target.files[0])}}
+                onChange={(e) => {
+                  setFile(e.target.files[0]);
+                }}
               />
               <br />
 
-              {photoUploadWarning?<><br /><p style={{color:"orangered",fontSize:"12px"}}>Enter student's name to upload photo</p></>:<></>}
-              <Button onClick={handleUpload}>Upload</Button>
+              {photoUploadWarning ? (
+                <>
+                  <br />
+                  <p style={{ color: "orangered", fontSize: "12px" }}>
+                    Enter student's name to upload photo
+                  </p>
+                </>
+              ) : (
+                <></>
+              )}
+              <Button
+                disabled={uploaded || !File}
+                variant={uploaded ? "success" : "primary"}
+                onClick={handleUpload}
+              >
+                {uploaded ? (
+                  <><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
+                  <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0"/>
+                  <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
+                </svg></>
+                ) : (
+                  <>Upload
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-cloud-arrow-up"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                       fillRule="evenodd"
+                        d="M7.646 5.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708z"
+                      />
+                      <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383m.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z" />
+                    </svg>
+                  </>
+                )}
+              </Button>
             </Form.Group>
           </Row>
           <br />
