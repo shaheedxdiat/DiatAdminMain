a2104efe50b6003cd5ca7b92d8f6eb0b804ed9a0
raheemudheen M A <raheemudheenma118@gmail.com>
2024-03-16 18:25:53 +0530

    Update AdminTitle.jsx

--- src/components/AdminTitle.jsx
+++ src/components/AdminTitle.jsx
@@ -3,8 +3,6 @@ import React from 'react'
 
 const AdminTitle = () => {
         const admin=localStorage.getItem("admin")
-
-   
   return (
     <div style={{position:"fixed" ,bottom:"2px",left:"2px"}}>
       <div style={{width:"100%", color:"#292c3244"}}>{admin}</div>
