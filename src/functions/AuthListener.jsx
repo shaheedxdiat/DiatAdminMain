4a8f48a5e1a5dcb7e3e87160622252a7590fc797
RAHEEMUDHEEN-MA <raheemudheenma118@gamil.com>
2024-03-22 19:45:33 +0530

    session timeout implemented (bug fix2)

--- src/functions/AuthListener.jsx
+++ src/functions/AuthListener.jsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../SupaBase";
 
