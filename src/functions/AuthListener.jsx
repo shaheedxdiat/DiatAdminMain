8f5c067b432b9f8d46dc4b43ef72c30bbde45a08
raheemudheen M A <raheemudheenma118@gmail.com>
2024-03-22 22:52:28 +0530

    Update AuthListener.jsx

--- src/functions/AuthListener.jsx
+++ src/functions/AuthListener.jsx
@@ -4,25 +4,19 @@ import { useNavigate } from "react-router-dom";
 import { supabase } from "../SupaBase";
 
 const AuthListener = () => {
-
-
-  const navigate = useNavigate();
+const navigate = useNavigate();
 
   useEffect(() => {
-    
     const { data } = supabase.auth.onAuthStateChange((event, session) => {
       if (event === "SIGNED_OUT") {
         navigate("/");
         console.log("sign-out success ", data.subscription.unsubscribe.name);
         localStorage.clear()
-       
       }
       if (!session) {
         navigate("/");
       }
     });
-
-
   }, [navigate]);
 
   
