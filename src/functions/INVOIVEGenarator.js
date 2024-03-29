bc309290cfc7ca17a427ac18156137a69521add7
RAHEEMUDHEEN-MA <raheemudheenma118@gamil.com>
2024-03-29 15:51:54 +0530

    payment statement fixes

--- src/functions/INVOIVEGenarator.js
+++ src/functions/INVOIVEGenarator.js
@@ -8,7 +8,6 @@ const generateINVOICE = async (payment_data, student_data) => {
   let y = 10;
 
   const companyName = "DIALOGUE INSTITUTE OF ADVANCED TECHNOLOGY";
-  
 
   doc.setFontSize(15);
   doc.text(companyName, doc.internal.pageSize.width / 2, 1.5 * y, {
@@ -18,7 +17,6 @@ const generateINVOICE = async (payment_data, student_data) => {
   doc.setDrawColor(0, 0, 0, 1);
   doc.setLineWidth(0.2);
   doc.line(10, 35, 200, 35);
-
 
   doc.setDrawColor(0, 0, 0, 0.5);
 
@@ -52,20 +50,17 @@ const generateINVOICE = async (payment_data, student_data) => {
   );
 
   // ---------
-const billdate=payment_data[0].created_at.toString().split("T")[0]
+  const billdate = payment_data[0].created_at.toString().split("T")[0];
 
-const parts1 = billdate.split("-"); 
-const formattedbilldate = `${parts1[2]}-${parts1[1]}-${parts1[0]}`; 
+  const parts1 = billdate.split("-");
+  const formattedbilldate = `${parts1[2]}-${parts1[1]}-${parts1[0]}`;
 
   doc.text("Bill Date", doc.internal.pageSize.width - 70, 5 * y, {
     align: "left",
   });
-  doc.text(
-    `: ${formattedbilldate}`,
-    doc.internal.pageSize.width - 35,
-    5 * y,
-    { align: "left" }
-  );
+  doc.text(`: ${formattedbilldate}`, doc.internal.pageSize.width - 35, 5 * y, {
+    align: "left",
+  });
 
   doc.text("Bill No ", doc.internal.pageSize.width - 70, 6 * y, {
     align: "left",
@@ -97,8 +92,6 @@ const formattedbilldate = `${parts1[2]}-${parts1[1]}-${parts1[0]}`;
   doc.text("Amount", doc.internal.pageSize.width - 25, 73, {
     align: "left",
   });
-
-
 
   doc.setTextColor(0, 0, 0);
 
@@ -135,13 +128,13 @@ const formattedbilldate = `${parts1[2]}-${parts1[1]}-${parts1[0]}`;
   doc.setLineWidth(0.1);
   doc.line(10, 105, 200, 105);
 
-  const amount = payment_data[0].amount; 
+  const amount = payment_data[0].amount;
 
-  const TaxableAmount=Math.floor(amount * 0.82)
+  const TaxableAmount = Math.floor(amount * 0.82);
 
   doc.text(`GST`, doc.internal.pageSize.width - 80, 111);
   doc.text(
-    `${Math.floor(amount-TaxableAmount)}.00`,
+    `${Math.floor(amount - TaxableAmount)}.00`,
     doc.internal.pageSize.width - 12,
     111,
     {
@@ -165,73 +158,55 @@ const formattedbilldate = `${parts1[2]}-${parts1[1]}-${parts1[0]}`;
 
   const words = numberToWords.toWords(amount);
 
-
   doc.setFont("helvetica", "normal");
   doc.text(`Total In Words `, doc.internal.pageSize.width / 13, 131);
   doc.text(`: Rupees  ${words} only`, doc.internal.pageSize.width - 150, 131);
   doc.setLineWidth(0.1);
 
   doc.text(`Balance Fee `, doc.internal.pageSize.width / 13, 141);
- if (student_data[0].fee_due===0) {
-  doc.text(
-    `: Fee Completed`,
-    doc.internal.pageSize.width - 150,
-    141
-  );
-
- }else  {
-  doc.text(
-    `: ${student_data[0].fee_due}`,
-    doc.internal.pageSize.width - 150,
-    141
-  );
- }
-  const casier=payment_data[0].cashier
+  if (student_data[0].fee_due === 0) {
+    doc.text(`: Fee Completed`, doc.internal.pageSize.width - 150, 141);
+  } else {
+    doc.text(
+      `: ${student_data[0].fee_due}`,
+      doc.internal.pageSize.width - 150,
+      141
+    );
+  }
+  const casier = payment_data[0].cashier;
 
   var atIndex = casier.indexOf("@");
 
-var username = casier.substring(0, atIndex);
+  var username = casier.substring(0, atIndex);
 
-doc.setFontSize(9)
-doc.setTextColor(128, 128, 128);
+  doc.setFontSize(9);
+  doc.setTextColor(128, 128, 128);
   doc.text(`Cashier`, doc.internal.pageSize.width - 80, 153);
-  doc.text(
-    ` ${username}`,
-    doc.internal.pageSize.width - 40,
-    153
-  );
+  doc.text(` ${username}`, doc.internal.pageSize.width - 35, 153);
   doc.line(10, 147, 200, 147);
 
-  var currentDate = new Date();
+  doc.setFontSize(10);
+  doc.setTextColor(128, 128, 128);
 
 
-  var formattedDate =
-    currentDate.getFullYear() +
-    "-" +
-    ("0" + (currentDate.getMonth() + 1)).slice(-2) +
-    "-" +
-    ("0" + currentDate.getDate()).slice(-2);  
-    doc.setFontSize(10)
-    doc.setTextColor(128, 128, 128);
+  const printedDate = new Date().toLocaleDateString("en-GB");
+  const printedTime = new Date().toLocaleTimeString("en-US", { hour12: true });
+  doc.text(
+    "Printed: " + printedDate + " " + printedTime,
+    doc.internal.pageSize.width / 13,
+    153
+  );
 
-
-    
-const parts = formattedDate.split("-"); 
-const printedDate = `${parts[2]}-${parts[1]}-${parts[0]}`;
-doc.setFontSize(9)
-  doc.text("Printed Date", doc.internal.pageSize.width / 13, 153);
-  doc.text(printedDate, doc.internal.pageSize.width - 150, 153);
-  doc.setFontSize(10)
+  doc.setFontSize(10);
   doc.setTextColor(0, 0, 0);
   doc.text("Signature & Stamp", doc.internal.pageSize.width - 50, 240);
-  
+
   doc.setDrawColor(0, 0, 0, 0.5);
 
   doc.setLineWidth(0.2);
-  doc.line(10, 260, 200,260);
+  doc.line(10, 260, 200, 260);
 
-
-  doc.addImage(footer, "PNG", 30, 255, doc.internal.pageSize.width-60, y*3);
+  doc.addImage(footer, "PNG", 30, 255, doc.internal.pageSize.width - 60, y * 3);
 
   doc.save(`DIAT_INVOICE_000${payment_data[0].id}.pdf`);
 };
--- src/functions/StatementGenarator.js
+++ src/functions/StatementGenarator.js
@@ -2,44 +2,47 @@ import React from 'react';
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
 
-
-const StatementGenerator =async (log,id) => {
- console.log(log);
+const StatementGenerator = async (log, id) => {
+  console.log(log);
   const doc = new jsPDF();
 
-        
-  doc.setFontSize(16);
+  doc.setFontSize(15);
   doc.setTextColor(128, 128, 128);
-  doc.text(`STATEMENT - ${id}`, 15, 14);
+  doc.text(`PAYMENT SUMMARY - ${id}`, 60, 13);
 
   doc.setTextColor(0, 0, 0);
 
-  const headers = [['S.No', 'Date', 'Amount','Description', 'Casier']];
+  const headers = [['S.No', 'Date', 'Amount', 'Description', 'Casier']];
 
-  const rows = log.map((statement,index) => [index+1, statement.created_at.split("T")[0], statement.amount,statement.remark, statement.cashier]);
-
+  const rows = log.map((statement, index) => [
+    index + 1,
+    statement.created_at.split('T')[0],
+    statement.amount,
+    statement.remark,
+    statement.cashier.split('@')[0],
+  ]);
 
   doc.autoTable({
     startY: 20,
     head: headers,
     body: rows,
     didDrawPage: function (data) {
-        const pageNumber = data.pageNumber;
-        doc.setTextColor(128, 128, 128);
+      const pageNumber = data.pageNumber;
+      doc.setTextColor(148, 148, 148);
+      doc.setFontSize(10);
+      doc.text('Page ' + pageNumber, data.settings.margin.right, doc.internal.pageSize.height - 10);
 
-        doc.setFontSize(10);
-        doc.text('Page ' + pageNumber, data.settings.margin.right, doc.internal.pageSize.height - 10);
-      }
+     
+      const printedDate = new Date().toLocaleDateString('en-GB');
+      const printedTime = new Date().toLocaleTimeString('en-US', { hour12: true });
+      doc.text('Printed: ' + printedDate + ' ' + printedTime, data.settings.margin.left+14, doc.internal.pageSize.height - 10);
+
+       },
   });
-
 
   doc.save(`STATEMENT_${id}.pdf`);
 
-  return (
-    <div>
-     
-    </div>
-  );
+  return <div></div>;
 };
 
 export default StatementGenerator;
